9d1cda1c99c8a5d8c98e0dbde29e375be313bdb9
admin <admin@admindeMac-Pro-2.local>
2021-05-25 14:50:31 +0800

    更新内容

---
+++ 数组系列/array_intersect.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("JGO师--gogogo\n")
+	var array1 = []int{1, 3, 3, 5, 8}
+	var array2 = []int{1, 3, 5}
+	var result = []int{}
+	result = intersect(array1, array2)
+	for index, value := range result {
+		fmt.Printf("%v：%v\n", index, value)
+	}
+}
+
+/*
+ 两数组交集获取
+ @params nums1
+ @params nums2
+*/
+func intersect(nums1 []int, nums2 []int) []int {
+	m0 := map[int]int{}
+	for _, v := range nums1 {
+		//遍历nums1，初始化map
+		m0[v] += 1
+	}
+	k := 0
+	for _, v := range nums2 {
+		//如果元素相同，将其存入nums2中，并将出现次数减1
+		if m0[v] > 0 {
+			m0[v] -= 1
+			nums2[k] = v
+			k++
+		}
+	}
+	return nums2[0:k]
+}
---
+++ 数组系列/array_intersect.js
@@ -0,0 +1,24 @@
+// *** Main-test
+let array1 = [1, 3, 5]
+let array2 = [1, 3, 5]
+
+let array3 = getIntersect(array1, array2);
+printInfo(array3);
+
+
+// *** 方法区
+/**
+ * 获取两数组交集
+ */
+function getIntersect(array1, array2) {
+    for() {
+        
+    }
+}
+
+/**
+ * 打印信息
+ */
+function printInfo(array3) {
+    console.log("info:" + array3);
+}

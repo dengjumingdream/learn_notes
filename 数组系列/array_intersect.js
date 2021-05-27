d2c81ae50681935268baf562b2aa00840942498d
admin <admin@admindeMac-Pro-2.local>
2021-05-27 13:56:44 +0800

    5月27日。。。

--- 数组系列/array_intersect.js
+++ 数组系列/array_intersect.js
@@ -1,6 +1,6 @@
 // *** Main-test
-let array1 = [1, 3, 5]
-let array2 = [1, 3, 5]
+let array1 = [1, 3, 3, 5]
+let array2 = [1, 3, 3, 5, 7]
 
 let array3 = getIntersect(array1, array2);
 printInfo(array3);
@@ -11,9 +11,27 @@ printInfo(array3);
  * 获取两数组交集
  */
 function getIntersect(array1, array2) {
-    for() {
-        
+    let map1 = {}
+    for(let i = 0, len = array1.length; i <len; i++) {
+        let _temp1 = array1[i]
+        if(map1[_temp1]) {
+            map1[_temp1] += 1
+        }else {
+            map1[_temp1] = 1
+        }
     }
+
+    let resultArray = []
+    for(let index in map1) {
+        for(let i = 0, len = array2.length; i < len; i++) {
+            let _temp2 = array2[i]
+            if(index == _temp2 && map1[index] > 0) {
+                resultArray.push(index);
+                map1[index] -= 1 
+            }
+        }
+    }
+    return resultArray;
 }
 
 /**

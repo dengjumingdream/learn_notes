81c0621cd3a8846fc37fc99b5c509a394b0c5be5
dengjuming <juming.deng@leishenhuyu.com>
2021-06-07 14:25:49 +0800

    update 信息

--- 数组系列/common_prefix.js
+++ 数组系列/common_prefix.js
@@ -1,14 +1,24 @@
 let strArray = ["flower", "fly", "flow"]
 let firstStr = strArray[0];
 
-let result = getCommonPrefix(firstStr, strArray);
+let result = {
+    commonStr: ""
+}
+
+// 引用传递
+getCommonPrefix(firstStr, strArray, result);
 console.log(result);
+
+// 值传递
+let testStr = "A";
+getChangeStr(testStr);
+console.log(testStr);
 
 /**
  * 获取字符串数组公共前缀
  * @param array
  */
-function getCommonPrefix(firstStr, array) {
+function getCommonPrefix(firstStr, array, obj) {
     let len = array.length;
     if(len > 0) {
         let _subStr = firstStr.substring(0, firstStr.length - 1);
@@ -16,14 +26,18 @@ function getCommonPrefix(firstStr, array) {
             let tempStr = array[i];
             if(!tempStr.indexOf(_subStr)) {
                 if(_subStr.length > 0) {
-                    getCommonPrefix(_subStr, array);
+                    getCommonPrefix(_subStr, array, obj);
                 }else {
-                    return "";
+                    "";
                 }
             }
-            return _subStr
+            obj.commonStr = _subStr
         }
     }else {
-        return "";
+        "";
     }
+}
+
+function getChangeStr(str) {
+    str = "B";
 }

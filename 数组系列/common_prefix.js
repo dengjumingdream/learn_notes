e20910669c2d3635ff3e07a6856a5970884a42ae
admin <admin@admindeMac-Pro-2.local>
2021-06-07 14:25:09 +0800

    feature 完成公共前缀算法

--- 数组系列/common_prefix.js
+++ 数组系列/common_prefix.js
@@ -1,29 +1,33 @@
-let strArray = ["flower", "fly", "flow"]
+let strArray = ["fl", "fly", "flow"]
 let firstStr = strArray[0];
 
-let result = getCommonPrefix(firstStr, strArray);
+
+let result = {
+    commonStr: ""
+}
+getCommonPrefix(firstStr, strArray, result);
 console.log(result);
 
 /**
  * 获取字符串数组公共前缀
  * @param array
  */
-function getCommonPrefix(firstStr, array) {
+function getCommonPrefix(firstStr, array, result) {
     let len = array.length;
     if(len > 0) {
-        let _subStr = firstStr.substring(0, firstStr.length - 1);
         for(let i = 1; i < len; i++) {
             let tempStr = array[i];
-            if(!tempStr.indexOf(_subStr)) {
-                if(_subStr.length > 0) {
-                    getCommonPrefix(_subStr, array);
+            if(tempStr.indexOf(firstStr) == -1) {
+                if(firstStr.length > 0) {
+                    firstStr = firstStr.substring(0, firstStr.length - 1);
+                    getCommonPrefix(firstStr, array, result);
                 }else {
-                    return "";
+                    result.commonStr = "没找到";
                 }
             }
-            return _subStr
         }
+        result.commonStr = "找到了：" + firstStr
     }else {
-        return "";
+        result.commonStr = "没找到";
     }
 }
